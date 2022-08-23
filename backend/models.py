f3f769fbbf9e69d2e73ce5f8c12ae1a1e2ceaeff
JiatongLiu97 <jiatongliu97@outlook.com>
2022-08-23 01:31:47 -0700

    final commit

--- backend/models.py
+++ backend/models.py
@@ -2,9 +2,9 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
-database_name = "trivia"
-database_path = "postgresql://postgres:Ljt970719@localhost:5432/{}".format(database_name)
+database_path = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@localhost:5432/{}".format(DB_NAME)
 
 
 db = SQLAlchemy()
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
 from flaskr import create_app
 from models import setup_db, Question, Category
@@ -15,7 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://postgres:Ljt970719@localhost:5432/{}".format(self.database_name)
+        self.database_path = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@localhost:5432/{}".format(self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
